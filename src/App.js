74dcaf07de29e5758dec2a3884dec606cb9e0948
PreethikhaS <preethikhasundarabharathy@gmail.com>
2022-11-22 22:16:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {ExpenseItem} from "../src/components/ExpenseItem"
+
+function App() {
+  return (
+    <div >
+    <ExpenseItem/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,10 @@
+import  './expenseItem.css';
+
+export function ExpenseItem(){
+
+return(
+<div>
+<h1>Hello</h1>
+</div>
+)
+}
