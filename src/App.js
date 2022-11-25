3db5ec272eff4b1cdba67868abae7f1ee88fa3e6
PreethikhaS <preethikhasundarabharathy@gmail.com>
2022-11-25 19:25:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
 import {ExpenseItem} from "../src/components/ExpenseItem"
 
 function App() {
+  const expenses = [
+    {
+      id:'e1',
+      title: 'phone',
+      amount: 50000,
+      date: new Date(2022, 5, 11),
+    },
+    {
+       id:'e2',
+      title: 'Camera',
+      amount: 60000,
+      date: new Date(2022,5,12),
+    },
+    {
+       id:'e3',
+      title: 'laptop',
+      amount: 90000,
+      date: new Date(2022,5,.21),
+
+    }
+  ];
   return (
     <div >
-    <ExpenseItem/>
+    <ExpenseItem
+    title={expenses[0].title}
+    amount={expenses[0].amount}
+    date={expenses[0].date}
+    />
+    <ExpenseItem
+    title={expenses[1].title}
+    amount={expenses[1].amount}
+    date={expenses[1].date}
+    />
+    <ExpenseItem
+    title={expenses[2].title}
+    amount={expenses[2].amount}
+    date={expenses[2].date}
+    />
      
     </div>
   );
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,15 @@
-import  './expenseItem.css';
+import "./expenseItem.css";
+import { ExpenseDate } from "./ExpenseDate";
 
-export function ExpenseItem(){
+export function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
 
-return(
-<div>
-<h1>Hello</h1>
-</div>
-)
+        <div className="expense-item_price">Rs{props.amount}</div>
+      </div>
+    </div>
+  );
 }
